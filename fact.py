95bfc267739b4183024d30ba7d6be7c4f2e39e10
Soumodip Chandra <123438145+IamPiklu@users.noreply.github.com>
2024-05-15 14:12:00 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -2,7 +2,7 @@ import requests
 
 def get_fact():
     api_url = 'https://api.api-ninjas.com/v1/facts'
-    response = requests.get(api_url, headers={'X-Api-Key': 'a4X60rh1SD3ni+sUYUx75Q==G8Yt6YgPppIiBsyX'})
+    response = requests.get(api_url, headers={'X-Api-Key': 'api-ninjas Key'})
     if response.status_code == requests.codes.ok:
         fact = response.json()[0]['fact']  # Extract the fact from the response
         return fact
